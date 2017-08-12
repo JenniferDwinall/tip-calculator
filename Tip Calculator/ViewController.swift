9cb71ea28d85b2e9b2fabf5979245bd3e94df33d
Jennifer Dwinall <Jennifer.Dwinall@gmail.com>
2017-08-11 21:15:30 -0400

    Build the initial Tip Calculator

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billText: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let bill = Double(billText.text!) ?? 0
+        let tip = bill * Double(tipPercentages[tipControl.selectedSegmentIndex])
+        let total = bill + tip
 
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    
+    }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
